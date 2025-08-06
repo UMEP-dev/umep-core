29d3ce67f853a9e910344c8f6d4e5d46f89701b5
Gareth Simons <garethsimons@me.com>
2025-08-06 14:54:57 +0100

    fixes date parsing for EPW start and end dates

--- umep/functions/SOLWEIGpython/solweig_runner_core.py
+++ umep/functions/SOLWEIGpython/solweig_runner_core.py
@@ -106,17 +106,21 @@ class SolweigRunCore(SolweigRun):
     def load_epw_weather(self) -> WeatherData:
         """Load weather data from an EPW file."""
         epw_df, epw_info = read_epw(self.config.epw_path)
+        # Get timezone from epw_df index if present
+        tz = epw_df.index.tz
         start_date = pd.Timestamp(
             year=self.config.epw_start_date[0],
             month=self.config.epw_start_date[1],
             day=self.config.epw_start_date[2],
             hour=self.config.epw_start_date[3],
+            tzinfo=tz,
         )
         end_date = pd.Timestamp(
             year=self.config.epw_end_date[0],
             month=self.config.epw_end_date[1],
             day=self.config.epw_end_date[2],
             hour=self.config.epw_end_date[3],
+            tzinfo=tz,
         )
         # Filter by date range
         filtered_df = epw_df.loc[start_date:end_date]
